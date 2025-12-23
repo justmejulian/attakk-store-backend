da483e4389a12e918c7d13eecaa53c10d4a3de59
Julian Visser <julian.visser@husqvarnagroup.com>
2025-12-23 20:44:38 +0100

    Init

---
+++ .prettierrc.mjs
@@ -0,0 +1,7 @@
+export default {
+  singleQuote: true,
+  trailingComma: 'es5',
+  tabWidth: 2,
+  semi: false,
+  printWidth: 100,
+};
---
+++ eslint.config.js
@@ -0,0 +1,21 @@
+import js from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default [
+  js.configs.recommended,
+  ...typescriptEslint.configs.recommended,
+  {
+    files: ['src/**/*.ts'],
+    languageOptions: {
+      parser: tseslint.parser,
+      parserOptions: {
+        project: './tsconfig.json',
+      },
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/explicit-function-return-type': 'off',
+      '@typescript-eslint/no-explicit-any': 'error',
+    },
+  },
+];
